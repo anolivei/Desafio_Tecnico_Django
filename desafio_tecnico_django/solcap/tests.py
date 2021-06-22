191f057d498554cd03ef8e03af45b1e9f2db667c
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-06-22 12:24:07 -0300

    testing

--- desafio_tecnico_django/solcap/tests.py
+++ desafio_tecnico_django/solcap/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from desafio_tecnico_django.solcap.models import Person
+from django.urls import reverse
 
-# Create your tests here.
+class PersonTestCase(APITestCase):
+	def setUp(self):
+		self.list_url = reverse('Person-list')
+		self.person1 = Person.objects.create(
+					nome='Cezar Augusto',
+					cpf='111.222.333-44',
+					rg='11.222.333-4',
+					data_nasc='19/04/1986',
+					sexo='Masculino',
+					mae='Graça Maria',
+					pai='Castilho Mario',
+					celular='(31) 99999-9999',
+					altura='1,80',
+					peso='80',
+					tipo_sanguineo='AB+')
+
